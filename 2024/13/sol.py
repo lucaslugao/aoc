a5c6120ee37ebf65a19bd3c9436dc924cc2091d2
Lucas Lugao <lucaslugaoguimaraes@gmail.com>
2024-12-13 13:39:14 +0000

    Add linear algebra solution given that there is only one possible solution to the problem

--- 2024/13/sol.py
+++ 2024/13/sol.py
@@ -11,7 +11,7 @@ class Machine:
     button_b: tuple[int, int]
     prize: tuple[int, int]
 
-    def solve(self, problem):
+    def solve_ip(self, problem):
         solver = pywraplp.Solver.CreateSolver("SCIP")
         a = solver.IntVar(0, 100 if problem == 1 else math.inf, "a")
         b = solver.IntVar(0, 100 if problem == 1 else math.inf, "b")
@@ -23,11 +23,27 @@ class Machine:
         solver.Add(a * self.button_a[1] + b * self.button_b[1] == prize[1])
 
         status = solver.Solve()
-
         if status == pywraplp.Solver.OPTIMAL:
             return int(solver.Objective().Value())
         else:
             return 0
+
+    def solve_linear(self, problem):
+        a = self.button_a
+        b = self.button_b
+        p = self.prize
+        if problem == 2:
+            p = (p[0] + 10000000000000, p[1] + 10000000000000)
+        det = a[0] * b[1] - a[1] * b[0]
+        x = (p[0] * b[1] - p[1] * b[0]) / det
+        y = (a[0] * p[1] - a[1] * p[0]) / det
+        # check integrality and positivity
+        if x % 1 != 0 or y % 1 != 0 or x < 0 or y < 0:
+            return 0
+        # check limit for problem 1
+        if problem == 1 and (x > 100 or y > 100):
+            return 0
+        return int(3 * x + y)
 
 
 if __name__ == "__main__":
@@ -52,7 +68,7 @@ if __name__ == "__main__":
                 (b_x, b_y),
                 (prize_x, prize_y),
             )
-            solution1 += m.solve(1)
-            solution2 += m.solve(2)
+            solution1 += m.solve_linear(1)
+            solution2 += m.solve_linear(2)
     print(solution1)
     print(solution2)
